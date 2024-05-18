0b13f0285ad4202f971b14b4af5be662a5f89de8
Giovanni <pino_otto@yahoo.com>
2024-05-18 16:30:55 +0800

    wip

--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,17 @@ use std::{
 fn main() {
     // let mut start = 0;
 
-    let mut input_file = File::open("/tmp/test-file").unwrap();
+    let mut input_file = File::open("/tmp/test-openjdk7.tgz").unwrap();
     let file_size = input_file.metadata().unwrap().len();
     println!("file size = {}", file_size);
 
-    let chunk_size: usize = 10000;
+    let chunk_size: usize = 50_000_000;
     println!("chunk_size = {}", chunk_size);
 
     let chunk_num = (file_size / chunk_size as u64).try_into().unwrap();
     println!("chunks = {}", chunk_num);
 
-    let remain = (file_size % chunk_size as u64).try_into().unwrap();
+    let remain: usize = (file_size % chunk_size as u64).try_into().unwrap();
     println!("remaning bytes = {}", remain);
 
     for i in 0..chunk_num {
@@ -38,7 +38,13 @@ fn main() {
             .seek(SeekFrom::Start(start.try_into().unwrap()))
             .unwrap();
         let mut buf = vec![0; remain];
-        input_file.read_exact(&mut buf);
+        let result = input_file.read_exact(&mut buf);
+        // let result = input_file.read_to_end(&mut buf);
+        match result {
+            // Ok(n) => println!("final chunk read successfully {} bytes", n),
+            Ok(_) => println!("final chunk read successfully"),
+            Err(_) => println!("error reading the final chunk"),
+        }
 
         let mut output_file = File::create(format!("/tmp/output-file{}", chunk_num)).unwrap();
         output_file.write_all(buf.as_slice()).unwrap();

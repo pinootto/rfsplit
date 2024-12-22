f67b7de2d0e5cdffb9ed1ab1a006120f2eb92413
Giovanni <pino_otto@yahoo.com>
2024-12-22 18:18:22 +0800

    removed comments

--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,14 @@ use std::{
 
 /// A simple program to split a large file into multiple smaller files
 #[derive(Parser, Debug)]
-#[command(version, about, long_about = "split large file in small files")]
+#[command(version, about, long_about = "Split large file into small files")]
 struct Args {
     /// input file
     #[arg(short, long)]
     input_file: String,
 
     /// output file
-    #[arg(short, long)]
+    #[arg(short, long, default_value = "/tmp/rfsplit.out")]
     output_file: String,
 
     /// size of each small file
@@ -26,16 +26,16 @@ fn main() {
     let args = Args::parse();
     let mut input_file = File::open(args.input_file).unwrap();
     let file_size = input_file.metadata().unwrap().len();
-    println!("file size = {}", file_size);
+    // println!("file size = {}", file_size);
 
     let chunk_size: usize = args.chunk_size;
-    println!("chunk_size = {}", chunk_size);
+    // println!("chunk_size = {}", chunk_size);
 
     let chunk_num = (file_size / chunk_size as u64).try_into().unwrap();
-    println!("chunks = {}", chunk_num);
+    // println!("chunks = {}", chunk_num);
 
     let remain: usize = (file_size % chunk_size as u64).try_into().unwrap();
-    println!("remaning bytes = {}", remain);
+    // println!("remaning bytes = {}", remain);
 
     for i in 0..chunk_num {
         let start = chunk_size * i;
@@ -56,15 +56,9 @@ fn main() {
             .seek(SeekFrom::Start(start.try_into().unwrap()))
             .unwrap();
         let mut buf = vec![0; remain];
-        let result = input_file.read_exact(&mut buf);
-        // let result = input_file.read_to_end(&mut buf);
-        match result {
-            // Ok(n) => println!("final chunk read successfully {} bytes", n),
-            Ok(_) => println!("final chunk read successfully"),
-            Err(_) => println!("error reading the final chunk"),
-        }
+        let _ = input_file.read_exact(&mut buf);
 
-        let output_filename = String::from(args.output_file.as_str()) + &chunk_size.to_string();
+        let output_filename = String::from(args.output_file.as_str()) + &chunk_num.to_string();
         let mut output_file = File::create(output_filename).unwrap();
         output_file.write_all(buf.as_slice()).unwrap();
     }
